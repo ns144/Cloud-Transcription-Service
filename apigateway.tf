a943ea2312ae7d56279b646e9d31653dc5c51eb6
ns144 <nikolas.schaber@gmail.com>
2023-12-08 00:36:49 +0100

    removed cognito

--- apigateway.tf
+++ apigateway.tf
@@ -95,10 +95,10 @@ resource "aws_api_gateway_method" "options" {
   rest_api_id = aws_api_gateway_rest_api.transcription_gateway.id
   resource_id = aws_api_gateway_resource.root.id
   http_method = "OPTIONS"
-  #   authorization = "NONE"
+  authorization = "NONE"
 
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.demo.id
+  #authorization = "COGNITO_USER_POOLS"
+  #authorizer_id = aws_api_gateway_authorizer.demo.id
 }
 
 resource "aws_api_gateway_integration" "options_integration" {
