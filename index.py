cf0e36e52c2db1ec026931e3550e37bcb6a7c2d6
ns144 <nikolas.schaber@gmail.com>
2023-12-08 21:00:33 +0100

    test of parameters in lambda

--- index.py
+++ index.py
@@ -4,16 +4,16 @@ def lambda_handler(event, context):
 
     response = 'Hello Lambda'
 
-    if 'queryStringParameters' in event:
+    if event['body']:
         print(event)
-        query_parameters = event['queryStringParameters']
+        body = event['body']
         
         # Access 'username' and 'password' from query parameters
-        username = query_parameters.get('username')
-        password = query_parameters.get('password')
+        #username = query_parameters.get('username')
+        #password = query_parameters.get('password')
 
-        response += username
-        response += password
+        response += body
+        #response += password
 
         return { 'statuscode' : 200, 'body' : json.dumps(response)}
     
