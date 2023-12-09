7f0a98b74d2060427ec66ff5f4e28466d0013bd6
ns144 <nikolas.schaber@gmail.com>
2023-12-09 20:43:10 +0100

    added reasonable response message

--- index.py
+++ index.py
@@ -19,19 +19,25 @@ def lambda_handler(event, context):
     instance = ec2.describe_instances(InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]
     current_state = instance['State']['Name']
 
+    response_msg = ""
+
     # Start or stop the instance based on its current state
     if current_state == 'stopped':
         print(f"Starting instance: {instance_id}")
         ec2.start_instances(InstanceIds=[instance_id])
         print(f"Instance started: {instance_id}")
+        response_msg = f"Instance started: {instance_id}"
     elif current_state == 'running':
         print(f"Stopping instance: {instance_id}")
         ec2.stop_instances(InstanceIds=[instance_id])
         print(f"Instance stopped: {instance_id}")
+        response_msg = f"Instance stopped: {instance_id}"
     else:
         print(f"Unsupported instance state: {current_state}")
+        response_msg = f"Unsupported instance state: {instance_id}"
 
     print(f"Current instance state: {current_state}")
 
+    response += response_msg
 
     return { 'statusCode' : 200, 'body' : json.dumps(response)}
