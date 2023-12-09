fc3904fb2acaf28cf562f8fce5b20ed929098219
ns144 <nikolas.schaber@gmail.com>
2023-12-09 18:19:59 +0100

    Initial EC2 setup

---
+++ ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "transcription_server" {
+# AMI Ubuntu Server 22.04 LTS X86 
+  ami           = "ami-06dd92ecc74fdfb36"
+
+  instance_type = "t2.micro"
+
+  # Should make shure that ec2 is not running initially
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Instance initialized but not started."
+              EOF  
+
+  tags = {
+
+    Name = "Transcription Server"
+
+  }
+
+}
