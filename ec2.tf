821c2f3feb1a0edbcb3461830ea74ec0189bd85c
ns144 <nikolas.schaber@gmail.com>
2023-12-21 19:43:25 +0100

    Fix: Removed leading space

--- ec2.tf
+++ ec2.tf
@@ -55,19 +55,19 @@ resource "aws_iam_role" "ec2_s3_role" {
   name = "ec2_s3_role"
   
   assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "s3_access" {
