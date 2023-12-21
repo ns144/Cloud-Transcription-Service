3f65e96e7134eed1aee1e460674a881cf3a84ad0
ns144 <nikolas.schaber@gmail.com>
2023-12-21 01:10:42 +0100

    Setup of init file for ec2 init

--- ec2.tf
+++ ec2.tf
@@ -8,68 +8,7 @@ resource "aws_instance" "transcription_server" {
   associate_public_ip_address = true
 
   # Should make shure that ec2 is not running initially
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Instance initialized but not started."
-
-              # Install git
-              sudo apt-get update
-              sudo apt-get install -y git
-
-              # Set the repository URL and target directory
-              repo_url="https://github.com/hyqshr/whispercpp-fastapi.git"
-              target_dir="/whispercpp-fastapi"
-
-              # Check if the target directory exists and is a Git repository
-              if [ -d "$target_dir/.git" ]; then
-                # If it's a Git repository, pull the latest changes
-                cd "$target_dir"
-                git pull origin main
-              else
-                # If it's not a Git repository, clone the repository
-                git clone "$repo_url" "$target_dir"
-                
-                # Navigate to the repository directory
-                cd "$target_dir"
-                fi
-
-              # Install requirements
-              sudo apt-get install -y python3-pip
-              sudo apt install -y ffmpeg
-              pip install -r requirements.txt
-              sudo apt install nginx
-
-              cd /etc/nginx/sites-enabled/
-
-              # Start your application or any other setup steps
-              python3 -m uvicorn main:app --reload
-
-              # Configure Nginx server block
-              sudo tee /etc/nginx/sites-available/fastapi_nginx <<CONFIG
-              server {
-                listen 80;
-                server_name 3.87.220.60;
-                location / {
-                    proxy_pass http://127.0.0.1:8000;
-                }
-            
-              }
-              CONFIG
-
-              # Create a symbolic link to enable the server block
-              sudo ln -s /etc/nginx/sites-available/your_server_block /etc/nginx/sites-enabled/
-
-              # Remove default Nginx configuration
-              sudo rm /etc/nginx/sites-enabled/default
-
-              # Restart Nginx to apply changes
-              sudo service nginx restart
-              
-              # Start Python App
-              cd ~/whispercpp-fastapi
-              python3 -m uvicorn main:app --reload
-
-              EOF  
+  user_data = user_data = "${file("init.sh")}"  
 
   tags = {
 
---
+++ init.sh
@@ -0,0 +1,48 @@
+# Set the repository URL and target directory
+repo_url="https://github.com/hyqshr/whispercpp-fastapi.git"
+target_dir="/home/ubuntu/whispercpp-fastapi"
+
+# Check if the target directory exists and is a Git repository
+if [ -d "$target_dir/.git" ]; then
+# If it's a Git repository, pull the latest changes
+cd "$target_dir"
+git pull origin main
+echo "Repository pulled."
+else
+# If it's not a Git repository, clone the repository
+git clone "$repo_url" "$target_dir"
+                
+# Navigate to the repository directory
+cd "$target_dir"
+fi
+
+# Install requirements
+sudo apt-get install -y python3-pip
+sudo apt install -y ffmpeg
+pip install -r requirements.txt
+sudo apt install nginx
+
+public_ip=$(curl http://checkip.amazonaws.com)
+
+echo "Public IP: $public_ip"
+
+sudo rm -rf /etc/nginx/sites-available/fastapi_nginx
+
+# Configure Nginx server block
+sudo cat >/etc/nginx/sites-enabled/fastapi_nginx <<EOL
+server {
+    listen 80;
+    server_name $public_ip;
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+    }
+
+}
+EOL
+sudo cat /etc/nginx/sites-enabled/fastapi_nginx
+
+# Restart Nginx to apply changes
+sudo service nginx restart
+
+python3 -m uvicorn main:app --reload
+echo "App started"
