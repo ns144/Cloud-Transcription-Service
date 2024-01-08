b7355f0823355c2cda7ded8e88b7128b97ac62df
ns144 <nikolas.schaber@gmail.com>
2024-01-08 15:49:31 +0100

    Updated incorrect usage of Github Secret

--- secretmanager.tf
+++ secretmanager.tf
@@ -5,5 +5,5 @@ resource "aws_secretsmanager_secret" "transcription_service_api_key" {
 
 resource "aws_secretsmanager_secret_version" "transcription_service_api_key_version" {
  secret_id = aws_secretsmanager_secret.transcription_service_api_key.id
- secret_string = ${{secrets.TRANSCRIPTION_SERVICE_API_KEY}}
+ secret_string = var.TRANSCRIPTION_SERVICE_API_KEY
 }
