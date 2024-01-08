3f786f9e43d8970918370323eda302528a71d30b
ns144 <nikolas.schaber@gmail.com>
2024-01-08 16:26:57 +0100

    Another way to set var

--- secretmanager.tf
+++ secretmanager.tf
@@ -1,4 +1,4 @@
-variable "TRANSCRIPTION_SERVICE_API_KEY" {
+variable "transcription_api_key" {
   description = "TRANSCRIPTION_SERVICE_API_KEY"
   type        = string
 }
@@ -10,5 +10,5 @@ resource "aws_secretsmanager_secret" "transcription_service_api_key" {
 
 resource "aws_secretsmanager_secret_version" "transcription_service_api_key_version" {
  secret_id = aws_secretsmanager_secret.transcription_service_api_key.id
- secret_string = var.TRANSCRIPTION_SERVICE_API_KEY
+ secret_string = var.transcription_api_key
 }
