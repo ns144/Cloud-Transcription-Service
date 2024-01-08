9d3c7e2dee2e0852ffebfff9ed149c4413987669
ns144 <nikolas.schaber@gmail.com>
2024-01-08 16:50:43 +0100

    Rename var because of apply error

--- secretmanager.tf
+++ secretmanager.tf
@@ -3,12 +3,13 @@ variable "transcription_api_key" {
   type        = string
 }
 
-resource "aws_secretsmanager_secret" "transcription_service_api_key" {
+resource "aws_secretsmanager_secret" "api_key" {
  name = "transcription_service_api_key"
+ recovery_window_in_days = 0
 }
 
 
-resource "aws_secretsmanager_secret_version" "transcription_service_api_key_version" {
- secret_id = aws_secretsmanager_secret.transcription_service_api_key.id
+resource "aws_secretsmanager_secret_version" "api_key_version" {
+ secret_id = aws_secretsmanager_secret.api_key.id
  secret_string = var.transcription_api_key
 }
