1172e129928430e30574c0182e2238d4772f64b8
ns144 <nikolas.schaber@gmail.com>
2024-01-08 16:12:36 +0100

    Another attempt to set var

--- secretmanager.tf
+++ secretmanager.tf
@@ -1,7 +1,6 @@
 variable "TRANSCRIPTION_SERVICE_API_KEY" {
   description = "TRANSCRIPTION_SERVICE_API_KEY"
   type        = string
-  sensitive   = true
 }
 
 resource "aws_secretsmanager_secret" "transcription_service_api_key" {
