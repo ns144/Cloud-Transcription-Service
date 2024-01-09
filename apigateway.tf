aa551be696bb6c73b338bac58488f137ff586d51
ns144 <nikolas.schaber@gmail.com>
2024-01-09 01:27:39 +0100

    removed auth header

--- apigateway.tf
+++ apigateway.tf
@@ -112,7 +112,7 @@ resource "aws_api_gateway_integration_response" "proxy" {
 
   response_parameters = {
 
-    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'",
 
     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
 
