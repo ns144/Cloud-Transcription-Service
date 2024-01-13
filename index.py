3e9600445956ecdb3a5a35f48e44155426a26a6f
ns144 <nikolas.schaber@gmail.com>
2024-01-13 14:53:34 +0100

    Fixed Unsupported Instance State, waiting for EC2 to shutdown and restart it

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import json
 import base64
 import boto3
 import os
+import time
 
 def lambda_handler(event, context):
     print('## EXECUTION OF LAMBDA')
@@ -49,7 +50,25 @@ def lambda_handler(event, context):
         response_msg = f"Instance already running: {instance_id}"
     else:
         print(f"Unsupported instance state: {current_state}")
-        response_msg = f"Unsupported instance state: {instance_id}"
+        # This happens at the startup and shutdown of an Instance
+        unsupported = True
+        while unsupported:
+            time.sleep(5)
+            # Check the current state of the instance
+            instance = ec2.describe_instances(InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]
+            current_state = instance['State']['Name']
+            if current_state == 'stopped':
+                print(f"Starting instance: {instance_id}")
+                ec2.start_instances(InstanceIds=[instance_id])
+                print(f"Instance started: {instance_id}")
+                response_msg = f"Instance started: {instance_id}"
+                break
+            elif current_state == 'running':
+                response_msg = f"Instance already running: {instance_id}"
+                break
+            else:
+                print("Unsupported instance state. Trying again")
+            
 
     print(f"Current instance state: {current_state}")
 
