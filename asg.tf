5a48f6f8f69a508bdcaaa526333cc6a0b6a9a36a
Hannes Koksch <hannes.koksch@gmail.com>
2025-01-29 12:47:17 +0100

    set ASG max size to 10

--- asg.tf
+++ asg.tf
@@ -1,8 +1,7 @@
 resource "aws_autoscaling_group" "ton-texter-transcription-servers" {
     name = "ton-texter-transcription-servers"
     availability_zones = ["eu-central-1a", "eu-central-1b"]
-    desired_capacity   = 0
-    max_size           = 5
+    max_size           = 10
     min_size           = 0
 
     launch_template {
